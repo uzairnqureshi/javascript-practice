5030dac604e4915db34ac865815b9c33605dbde7
Uzair Qureshi <109924172+uzairnqureshi@users.noreply.github.com>
2024-07-30 18:15:05 +0000

    01

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountld = 122333
+let accountEmail = "uzairqureshi@gmail.com"
+var accountPassword = "1234"
+accountCity = "Islamabad"
+let state
+// accountld = 2
+accountEmail = "uzair@gmail.com"
+accountPassword = "12345"
+accountCity = "Multan"
+console.log (accountld)
+console.table([accountEmail, accountPassword, accountCity, state])
