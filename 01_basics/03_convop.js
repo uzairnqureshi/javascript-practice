5d25eced2a02eff14456dafcddd84df16b9c922b
Uzair Qureshi <109924172+uzairnqureshi@users.noreply.github.com>
2024-07-30 23:52:03 +0000

    03

---
+++ 01_basics/03_convop.js
@@ -0,0 +1,9 @@
+let number = 12
+let string = String(number)
+console.log(string)
+console.log(typeof string)
+
+let filledString = "abc"
+let boolean = Boolean(filledString)
+console.log(boolean)
+console.log(typeof boolean)
