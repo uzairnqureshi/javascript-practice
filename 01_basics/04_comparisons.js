cd5f3b1490404a37478995c7df3b566ebf928e6d
Uzair Qureshi <109924172+uzairnqureshi@users.noreply.github.com>
2024-07-31 04:02:50 +0000

    04

--- 01_basics/04_comparisons.js
+++ 01_basics/04_comparisons.js
@@ -1,3 +1,10 @@
-console.log("Hello")
+console.log(false < "1") //comparison operators coverts enititie into numbers while == doesn't
 
-console.log("Bye")
+console.log(null <= 1)
+console.log(null == 1)
+
+console.log(undefined == 0) //always undefined
+console.log(undefined >= 0)
+
+console.log(1 >= 0) //simple conversions
+console.log(1 < 2)
